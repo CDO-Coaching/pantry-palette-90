3deb30d608dbdb2198e6326d2b4ccdf3dfa04a81
CDO-Coaching <cdo.personaltrainer@gmail.com>
2025-10-02 15:05:42 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,14 @@ import react from "@vitejs/plugin-react-swc"
 import path from "path"
 import { componentTagger } from "lovable-tagger"
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     port: 8080,
   },
-  base: "/",
+  base: "/", // garde la racine
   build: {
     outDir: "dist",
+    assetsDir: "assets", // <-- ajoute ceci
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
@@ -19,4 +19,3 @@ export default defineConfig(({ mode }) => ({
     },
   },
 }))
-
